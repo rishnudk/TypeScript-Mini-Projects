4a8cbe10e4a0a0e5bb1c16e9319b36ceb7655c09
Rishnu DK <rishnukrishnan@gmail.com>
2025-07-25 23:03:05 +0530

    More Practice Added

---
+++ practice/types.ts
@@ -0,0 +1,73 @@
+
+// union  : A value can be any one of the specified types.
+
+// let value : string | number;
+
+// value = 'hello'
+// value = 123
+// value = true;
+
+
+//intersection  A value must have all types combined
+
+type person = { name: string};
+
+type Employee = { id: number};
+
+type Workerr = person  & Employee;
+
+const workerId : Workerr = {
+    name : 'rishnu',
+    id : 11
+}
+
+
+// extends    
+ //   means "inherits from" or "is based on" another type
+ //    If Type B extends Type A, then Type B gets everything from Type A, and can add more.
+
+interface Persons {
+    name : string;
+    age : number;
+}
+
+interface Empp extends Persons {
+    empId: number;
+}
+
+
+const e : Empp  = {
+    name : 'rishnu',
+    age : 11,
+    empId : 33
+}
+
+
+// extends wit h class
+
+class Animal {
+    move() {
+        console.log('moving')
+    }
+}
+
+class Dog extends Animal {
+    bark() {
+        console.log('barking')
+    }
+}
+
+const d = new Dog();
+d.move();
+d.bark();
+
+
+// optional properties 
+
+interface P {
+    name : string;
+    age? : number;
+}
+
+const p1 : P = { name : 'rishnu'}
+const p2 : P = {age : 12}
